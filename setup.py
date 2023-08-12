09ba0956ba49e3aa25d1e7c9059ee0d91a1ffa97
lkaijie <kaijie6@ualberta.ca>
2023-08-12 15:41:35 -0600

    0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ani-sched",
-    version="0.1.2",
+    version="0.1.3",
     description="A local MAL API for anime schedules and news (using livechart.me)",
     license="MIT",
     long_description=long_description,
@@ -21,5 +21,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.10",
 )
